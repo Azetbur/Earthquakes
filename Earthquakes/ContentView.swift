a1a81517e059f0c0547e3e04877369abd0437a6e
Jindrich Kocman <jindra.kocman@gmail.com>
2022-08-09 13:13:40 +0200

    Initial Commit

---
+++ Earthquakes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Earthquakes
+//
+//  Created by Jindrich Kocman on 09.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Jindrich Kocman on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
